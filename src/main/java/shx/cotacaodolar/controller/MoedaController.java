f7e595e50a919b67f3fa6ae6c61332312ffe5bae
Mepamaze <mepamaze@gmail.com>
2024-02-12 18:29:33 -0300

    Feat: New route to get dollar quote from any date
    Signed-off-by: Mepa <mepamazew@gmail.com>

--- src/main/java/shx/cotacaodolar/controller/MoedaController.java
+++ src/main/java/shx/cotacaodolar/controller/MoedaController.java
@@ -2,8 +2,11 @@ package shx.cotacaodolar.controller;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.sql.Date;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +26,21 @@ public class MoedaController {
     @Autowired
     private MoedaService moedaService;
 
-    @GetMapping("/moeda/{data1}&{data2}")
-    public List<Moeda> getCotacoesPeriodo(@PathVariable("data1") String startDate, @PathVariable("data2") String endDate) throws IOException, MalformedURLException, ParseException{
-        return moedaService.getCotacoesPeriodo(startDate, endDate);
+    @GetMapping("/moeda/cotacao/{data}")
+    public Moeda getCotacoesData(@PathVariable("data") String data) throws IOException, MalformedURLException, ParseException{
+        return moedaService.getCotacao(LocalDate.parse(data, DateTimeFormatter.ofPattern("MM-dd-yyyy")));
     }
 
     @GetMapping("/moeda/atual")
     public Moeda getCotacaoAtual() throws IOException, MalformedURLException, ParseException{
         return moedaService.getCotacao(null);
     }
-    
+
+    @GetMapping("/moeda/{data1}&{data2}")
+    public List<Moeda> getCotacoesPeriodo(@PathVariable("data1") String startDate, @PathVariable("data2") String endDate) throws IOException, MalformedURLException, ParseException{
+        return moedaService.getCotacoesPeriodo(startDate, endDate);
+    }
+
     // Desenvolver uma rotina que recebe um período e retorna uma lista de cotações, somente com as cotações menores que a cotação atual.
     @GetMapping("/moeda/cotacao-menor/{data1}&{data2}")
     public List<Moeda> getCotacoesMenoresAtual(@PathVariable("data1") String startDate, @PathVariable("data2") String endDate) throws IOException, MalformedURLException, ParseException{
